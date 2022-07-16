f2cfb7872d9a99d917363a544989699d7dc0712a
Jordane <jordan2offshel@hotmaile.com>
2022-07-16 11:20:04 -0400

    Mading By Zedrox

--- js/index.js
+++ js/index.js
@@ -11,12 +11,20 @@ const header = () => {
 
 header();
 
+const humb = document.querySelector(".humbger");
+const humbM = document.querySelector(".humbger__menu");
+
+humb.onclick = function(){
+    humb.classList.toggle("active");
+    humbM.classList.toggle("active");
+}
+
 var tle;
 
 tle = gsap.timeline({defauts: {ease: "power1.out"}});
 
 tle.to(".logo a", {opacity: 1, y: 0,duration: .5, stagger: .25, });
-tle.to(".navb a", {opacity: 1, y: 0,duration: .5, stagger: .25, });
 tle.to(".img__content", {opacity: 1, y: 0,duration: .5, stagger: .25, });
 tle.to(".data__content", {opacity: 1, x: 0,duration: .5, stagger: .25, });
 tle.to(".btn", {opacity: 1, y: 0,duration: .5, stagger: .25, });
+tle.to(".navb a", {opacity: 1, y: 0,duration: .5, stagger: .25, });
